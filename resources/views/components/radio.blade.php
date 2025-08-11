dc6a0d9eb951a3cfc4b7a7807d6205eb69aa2212
Christian Taylor <christianbtaylor@gmail.com>
2025-08-11 16:41:18 -0500

    Fix radio accent color

--- resources/views/components/radio.blade.php
+++ resources/views/components/radio.blade.php
@@ -2,7 +2,7 @@
     <input type="radio" {{ $controlAttributes }} value="{{ $value }}" {{ $attributes->class([
       'relative size-5 appearance-none rounded-full',
       'border border-gray-300 bg-white',
-      'before:absolute before:inset-1 before:rounded-full before:bg-white not-checked:before:hidden checked:border-(--color-accent-foreground) checked:bg-(--color-accent-foreground)',
+      'before:absolute before:inset-1 before:rounded-full before:bg-white not-checked:before:hidden checked:border-(--color-accent) checked:bg-(--color-accent)',
       'disabled:border-gray-300 disabled:bg-gray-100 disabled:before:bg-gray-400',
       'forced-colors:appearance-auto forced-colors:before:hidden',
     ]) }}>
