dade461f7f949211fb19d4a6a47f4a9d335782f6
Christian Taylor <christianbtaylor@gmail.com>
2025-08-11 11:03:13 -0500

    Update component styles

--- resources/views/components/radio.blade.php
+++ resources/views/components/radio.blade.php
@@ -1,3 +1,9 @@
 <x-has-inline-field>
-    <input type="radio" {{ $controlAttributes }} value="{{ $value }}" {{ $attributes->merge(['class' => 'h-5 w-5 text-teal-600 border-teal-300 focus:ring-teal-500']) }}>
+    <input type="radio" {{ $controlAttributes }} value="{{ $value }}" {{ $attributes->class([
+      'relative size-5 appearance-none rounded-full',
+      'border border-gray-300 bg-white',
+      'before:absolute before:inset-1 before:rounded-full before:bg-white not-checked:before:hidden checked:border-(--color-accent-foreground) checked:bg-(--color-accent-foreground)',
+      'disabled:border-gray-300 disabled:bg-gray-100 disabled:before:bg-gray-400',
+      'forced-colors:appearance-auto forced-colors:before:hidden',
+    ]) }}>
 </x-has-inline-field>
--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,3 +1,10 @@
 <x-has-field>
-    <textarea {{ $controlAttributes }} {{ $attributes->merge(['class' => 'shadow-sm block w-full focus:ring-teal-500 focus:border-teal-500 sm:text-sm border-gray-300 rounded-md']) }}>{{ $value ?: $slot }}</textarea>
+    <textarea {{ $controlAttributes }} {{ $attributes->class([
+      'appearance-none block w-full',
+      'bg-white dark:bg-white/5 dark:disabled:bg-white/2',
+      'text-gray-700 disabled:text-gray-500',
+      'rounded-lg border border-gray-200 border-b-gray-300/80 disabled:border-b-gray-200 dark:border-white/10 dark:disabled:border-white/5',
+      'shadow-xs disabled:shadow-none dark:shadow-none',
+      'aria-invalid:border-red-500',
+    ]) }}>{{ $value ?: $slot }}</textarea>
 </x-has-field>
