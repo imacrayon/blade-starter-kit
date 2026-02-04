63c82ebb8049ca6aa03b54659f1aef6cd628f5a3
Christian Taylor <christianbtaylor@gmail.com>
2026-02-04 11:39:49 -0600

    Clean up padding

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -5,16 +5,16 @@
 @php
 $classes = [
     'appearance-none block w-full',
-    'ps-3 pe-10',
+    'pe-10',
     'bg-white dark:bg-white/5 dark:disabled:bg-white/2',
     'text-gray-700 disabled:text-gray-500 placeholder-gray-400 disabled:placeholder-gray-400/70 dark:text-gray-300 dark:disabled:text-gray-400 dark:placeholder-gray-400 dark:disabled:placeholder-gray-500',
     'rounded-lg border border-gray-200 border-b-gray-300/80 disabled:border-b-gray-200 dark:border-white/10 dark:disabled:border-white/5',
     'shadow-xs disabled:shadow-none dark:shadow-none',
     'aria-invalid:border-red-500',
     match ($size) {
-        'base' => 'text-base sm:text-sm py-2 h-10 leading-[1.375rem]',
-        'sm' => 'text-sm py-1.5 h-8 leading-[1.125rem]',
-        'xs' => 'text-xs py-1.5 h-6 leading-[1.125rem]',
+        'base' => 'text-base sm:text-sm ps-3  py-2 h-10 leading-[1.375rem]',
+        'sm' => 'text-sm ps-2 py-1.5 h-8 leading-[1.125rem]',
+        'xs' => 'text-xs ps-1 py-1.5 h-6 leading-[1.125rem]',
     },
 ];
 @endphp
