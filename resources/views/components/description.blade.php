beb1081794252749721f854644513803aa9578a8
Christian Taylor <christianbtaylor@gmail.com>
2026-02-04 11:39:20 -0600

    Add description `for` attribute

--- resources/views/components/description.blade.php
+++ resources/views/components/description.blade.php
@@ -1,3 +1,7 @@
-<div {{ $attributes->class('text-sm text-gray-500 dark:text-white/60') }}>
+@props(['for'])
+
+<div {{ $attributes->class('text-sm text-gray-500 dark:text-white/60')->merge([
+    'id' => \App\View\Components\Control::sessionPath($for).'_description',
+  ]) }}>
     {{ $value ?? $slot }}
 </div>
