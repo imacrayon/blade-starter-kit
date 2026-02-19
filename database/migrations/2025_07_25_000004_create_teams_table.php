f60a0b285b2729843230310a00cd633d2f301a8e
Christian Taylor <christianbtaylor@gmail.com>
2026-02-19 09:53:32 -0600

    Add unique constraint to memberships

--- database/migrations/2025_07_25_000004_create_teams_table.php
+++ database/migrations/2025_07_25_000004_create_teams_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained('users');
             $table->string('role');
             $table->timestamps();
+            $table->unique(['team_id', 'user_id']);
         });
 
         Schema::table('users', function (Blueprint $table) {
